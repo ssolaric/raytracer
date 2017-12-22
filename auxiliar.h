7a38b4a538eaec439536517eed3b0b856a740cfc
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2017-12-22 12:13:28 -0500

    Primer commit

---
+++ auxiliar.h
@@ -0,0 +1,196 @@
+#ifndef AUXILIAR_H
+#define AUXILIAR_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_FORCE_RADIANS
+
+#define debug(x) do { std::cerr << #x << ": " << x << "\n"; } while (0)
+
+const float INF = std::numeric_limits<float>::max();
+const float PI = std::acos(-1.0f);
+const float EPS = 1e-8f;
+
+inline float deg2rad(float deg){ 
+    return deg * PI / 180;
+}
+
+// Esta función hace que v siempre caiga en el intervalo [lo, hi].
+inline float clamp(float lo, float hi, float v) {
+    return std::max(lo, std::min(hi, v));
+}
+
+std::ostream& operator<< (std::ostream& os, const glm::vec3& v) {
+    return os << '[' << v.x << ' ' << v.y << ' ' << v.z << ']';
+}
+
+struct Rayo {
+    glm::vec3 orig;
+    glm::vec3 dir;
+    Rayo(const glm::vec3& orig, const glm::vec3& dir): orig(orig), dir(dir) {}
+};
+
+struct Triangulo {
+    // Uso referencias a los vértices que se encuentran guardados en la malla
+    glm::vec3& v0;
+    glm::vec3& v1;
+    glm::vec3& v2;
+
+    glm::vec3 N; // vector normal (normalizado)
+    Triangulo(glm::vec3& v0, glm::vec3& v1, glm::vec3& v2): v0(v0), v1(v1), v2(v2) {
+        glm::vec3 v0v1 = v1 - v0;
+        glm::vec3 v0v2 = v2 - v0;
+        N = glm::normalize(glm::cross(v0v1, v0v2));
+        //debug(N);
+    }
+
+    bool intersect(const Rayo& rayo, float& t) const {
+        // t: parámetro en la ecuación paramétrica del rayo que indica el punto en que se encuentra la intersección.
+        // t también es la distancia desde el origen hasta el punto de intersección.
+
+        // 1. Calcular la normal del plano
+        // Esto se hizo en el constructor.
+
+        // 2. Calcular la intersección entre el rayo y el plano del triángulo (punto P)
+        float N_dot_R = glm::dot(N, rayo.dir);
+        
+        //debug(N_dot_R);
+        // Si la dirección del rayo y la normal del plano son perpendiculares
+        if (std::fabs(N_dot_R) < EPS) {
+            // Entonces el rayo y el plano son paralelos y no se intersectan.
+            return false;
+        }
+
+        t = glm::dot(N, v0 - rayo.orig) / N_dot_R;
+        //debug(t);
+        // 2.1. Si el plano está detrás del rayo, retornar falso.
+        if (t < 0) return false;
+        
+        // 2.2. Calcular P
+        glm::vec3 P = rayo.orig + t * rayo.dir;
+
+        // 3. Ver si el punto P está dentro del triángulo (inside-outside test)
+        glm::vec3 C;
+        
+        // Arista 0: del vértice 0 al vértice 1
+        glm::vec3 edge0 = v1 - v0;
+        glm::vec3 v0p = P - v0;
+        C = glm::cross(edge0, v0p);
+        if (glm::dot(N, C) < 0) return false;
+        
+        // Arista 1: del vértice 1 al vértice 2
+        glm::vec3 edge1 = v2 - v1;
+        glm::vec3 v1p = P - v1;
+        C = glm::cross(edge1, v1p);
+        if (glm::dot(N, C) < 0) return false;
+        
+        // Arista 2: del vértice 2 al vértice 0
+        glm::vec3 edge2 = v0 - v2;
+        glm::vec3 v2p = P - v2;
+        C = glm::cross(edge2, v2p);
+        if (glm::dot(N, C) < 0) return false;
+        
+        return true;
+    }
+};
+
+struct Mesh {
+    int num_vertices;
+    int num_triangulos;
+    std::vector<glm::vec3> vertices;
+    std::vector<Triangulo> triangulos;
+    glm::vec3 centro;
+    glm::mat4 matriz_transformacion; // object to world matrix
+
+    float scale;
+    glm::vec3 vec_maxz_malla; // (0, 0, maxz)
+
+    Mesh(const char* filename, const glm::mat4& matriz = glm::mat4(1.0f)): matriz_transformacion(matriz) {
+        std::ifstream ifs(filename);
+        if (!ifs) {
+            std::cerr << "No se encontró el archivo.\n";
+		    std::exit(1);
+        }
+        std::string cad;
+        ifs >> cad;
+        if (cad != "OFF") {
+            std::cerr << "Error de formato.\n";
+            std::exit(1);
+        }
+        int num_aristas; // no interesa
+        ifs >> num_vertices >> num_triangulos >> num_aristas;
+        vertices.reserve(num_vertices);
+
+        for (int i = 0; i < num_vertices; i++) {
+            ifs >> vertices[i].x >> vertices[i].y >> vertices[i].z;
+            centro.x += vertices[i].x;
+            centro.y += vertices[i].y;
+            centro.z += vertices[i].z;
+        }
+
+        for (int i = 0; i < num_triangulos; i++) {
+            int nv; // no interesa
+            int ind_v0, ind_v1, ind_v2;
+            ifs >> nv >> ind_v0 >> ind_v1 >> ind_v2;
+            triangulos.push_back(Triangulo(vertices[ind_v0], vertices[ind_v1], vertices[ind_v2]));
+        }
+        centro /= num_vertices;
+
+        float maxx = -INF, maxy = -INF, maxz = -INF;
+        float minx = INF, miny = INF, minz = INF;
+
+        for (int i = 0; i < num_vertices; i++) {
+            minx = std::min(minx, vertices[i].x);
+            maxx = std::max(maxx, vertices[i].x);
+            miny = std::min(miny, vertices[i].y);
+            maxy = std::max(maxy, vertices[i].y);
+            minz = std::min(minz, vertices[i].z);
+            maxz = std::max(maxz, vertices[i].z);
+        }
+        vec_maxz_malla = glm::vec3(0.0f, 0.0f, maxz);
+
+        float diag = std::sqrt((maxx - minx)*(maxx - minx) + (maxy - miny)*(maxy - miny) + (maxz - minz)*(maxz - minz));
+        scale = 2.0f / diag;
+    }
+
+    bool intersect(const Rayo& rayo, float& t_mas_cercano, int& indice_triangulo_mas_cercano) const {
+        bool hay_interseccion = false;
+        t_mas_cercano = INF;
+        for (int i = 0; i < num_triangulos; i++) {
+            float t = INF;
+            if (triangulos[i].intersect(rayo, t) && t < t_mas_cercano) {
+                t_mas_cercano = t;
+                hay_interseccion = true;
+                indice_triangulo_mas_cercano = i;
+            }
+        }
+        return hay_interseccion;
+    }
+
+    void transformar() {
+        // Fuerza la aplicación de la matriz de transformación a todos los vértices de la malla.
+        // Debido a que los triángulos guardan referencias de los vértices, sus vértices se actualizarán automáticamente.
+
+        // Mientras tanto, actualizar el maxz_malla
+        for (int i = 0; i < num_vertices; i++) {
+            vertices[i] = glm::vec3(matriz_transformacion * glm::vec4(vertices[i], 1.0f));
+        }
+        // Transformar también el centro:
+        centro = glm::vec3(matriz_transformacion * glm::vec4(centro, 1.0f));
+        vec_maxz_malla = glm::vec3(matriz_transformacion * glm::vec4(vec_maxz_malla, 1.0f));
+        // Transformar también las normales de los triángulos:
+        glm::mat3 mat_inv_transp = glm::transpose(glm::inverse(glm::mat3(matriz_transformacion)));
+        for (int i = 0; i < num_triangulos; i++) {
+            triangulos[i].N = glm::normalize(mat_inv_transp * triangulos[i].N);
+        }
+    }
+
+};
+
+#endif /* AUXILIAR_H */
---
+++ raytracer.cpp
@@ -0,0 +1,118 @@
+// Para compilar: g++ raytracer.cpp -o raytracer.out -I"./glm-0.9.7.3/glm" -O3
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <limits>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_FORCE_RADIANS
+
+#include "auxiliar.h"
+
+void guardar_imagen(const char* filename, glm::vec3* imagen, int width, int height) {
+    std::ofstream ofs(filename, std::ios::out | std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (int i = 0; i < height * width; i++) {
+        unsigned char r = (unsigned char) (255 * clamp(0.0f, 1.0f, imagen[i].x));
+        unsigned char g = (unsigned char) (255 * clamp(0.0f, 1.0f, imagen[i].y));
+        unsigned char b = (unsigned char) (255 * clamp(0.0f, 1.0f, imagen[i].z));
+        ofs << r << g << b;
+    }
+}
+
+glm::vec3 facing_ratio(const Rayo& rayo, const Triangulo& triangulo) {
+    return glm::vec3(std::max(0.0f, glm::dot(triangulo.N, -rayo.dir)));
+}
+
+glm::vec3 lanzar_rayo(const Rayo& rayo, const Mesh& mesh) {
+    glm::vec3 color_fondo(1.0, 0.0, 0.0); // rojo
+    float t_mas_cercano;
+    int indice_triangulo_mas_cercano;
+    if (mesh.intersect(rayo, t_mas_cercano, indice_triangulo_mas_cercano)) {
+        const Triangulo& triangulo_mas_cercano = mesh.triangulos[indice_triangulo_mas_cercano];
+        return facing_ratio(rayo, triangulo_mas_cercano) * glm::vec3(0.0f, 1.0f, 0.0f); // sombreado verde
+    }
+    else {
+        return color_fondo;
+    }
+}
+
+void prueba_malla() {
+    
+    Mesh malla("m1808.off");
+    
+    glm::mat4 mandar_al_centro = 
+        glm::scale(glm::mat4(1.0f), glm::vec3(malla.scale)) *
+        glm::translate(glm::mat4(1.0f), -malla.centro);
+    
+    glm::mat4 transformaciones_centro = 
+        glm::mat4(1.0f) *
+        glm::rotate(glm::mat4(1.0f), glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f)) *        
+        glm::rotate(glm::mat4(1.0f), glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    float d = 0.8f; // distancia deseada entre el plano de visión y la malla
+
+    malla.matriz_transformacion = 
+        transformaciones_centro *
+        mandar_al_centro;
+
+    malla.transformar();
+
+    glm::mat4 mandar_detras_del_plano = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -malla.vec_maxz_malla.z - 1 - d));
+    malla.matriz_transformacion = 
+        mandar_detras_del_plano;
+    malla.transformar();
+    
+    int width = 640;
+    int height = 480;
+    
+    glm::vec3* imagen = new glm::vec3[width * height];
+    glm::vec3* pixel = imagen;
+    
+    float fov = 90.0f;
+    float scale = std::tan(deg2rad(fov * 0.5f));
+    float aspect_ratio = width / (float) height;
+    
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++, pixel++) {
+            // transformar punto de la imagen en memoria a punto de la imagen en pantalla
+            float x1 = (2 * (i + 0.25) / (float)width - 1) * aspect_ratio * scale;
+            float y1 = (1 - 2 * (j + 0.25) / (float)height) * scale;
+            float x2 = (2 * (i + 0.75) / (float)width - 1) * aspect_ratio * scale;
+            float y2 = (1 - 2 * (j + 0.25) / (float)height) * scale;
+            float x3 = (2 * (i + 0.25) / (float)width - 1) * aspect_ratio * scale;
+            float y3 = (1 - 2 * (j + 0.75) / (float)height) * scale;
+            float x4 = (2 * (i + 0.75) / (float)width - 1) * aspect_ratio * scale;
+            float y4 = (1 - 2 * (j + 0.75) / (float)height) * scale;
+
+            glm::vec3 dir1(x1, y1, -1);
+            glm::vec3 dir2(x2, y2, -1);
+            glm::vec3 dir3(x3, y3, -1);
+            glm::vec3 dir4(x4, y4, -1);
+            Rayo rayo1(glm::vec3(0), glm::normalize(dir1));
+            Rayo rayo2(glm::vec3(0), glm::normalize(dir2));
+            Rayo rayo3(glm::vec3(0), glm::normalize(dir3));
+            Rayo rayo4(glm::vec3(0), glm::normalize(dir4));
+            glm::vec3 subpixel1 = lanzar_rayo(rayo1, malla);
+            glm::vec3 subpixel2 = lanzar_rayo(rayo2, malla);
+            glm::vec3 subpixel3 = lanzar_rayo(rayo3, malla);
+            glm::vec3 subpixel4 = lanzar_rayo(rayo4, malla);
+            *pixel = subpixel1 + subpixel2 + subpixel3 + subpixel4;
+            *pixel /= 4;
+        }
+    }
+    guardar_imagen("m1808.ppm", imagen, width, height);
+    delete[] imagen;
+}
+
+
+int main(int argc, char **argv) {
+    prueba_malla();
+    return 0;
+}
